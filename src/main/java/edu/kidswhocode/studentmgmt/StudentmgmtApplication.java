25676414c1bb896ab29d5245c26093aecf08e0d6
Hussain <sajhus@amazon.com>
2018-03-24 14:55:22 -0400

    Checking in LevelRepository and StudentRepository

--- src/main/java/edu/kidswhocode/studentmgmt/StudentmgmtApplication.java
+++ src/main/java/edu/kidswhocode/studentmgmt/StudentmgmtApplication.java
@@ -1,12 +1,31 @@
 package edu.kidswhocode.studentmgmt;
 
+import com.sun.org.apache.xml.internal.security.keys.keyresolver.implementations.PrivateKeyResolver;
+import edu.kidswhocode.studentmgmt.domain.Level;
+import edu.kidswhocode.studentmgmt.repository.LevelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentmgmtApplication {
 
+	@Autowired
+	private LevelRepository levelRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmgmtApplication.class, args);
+	}
+
+	public void generateLevels() {
+		Level level = new Level();
+		level.setName("KG");
+		levelRepository.save(level);
+
+		for (int i = 1; i <= 8; i++) {
+			level = new Level();
+			level.setName(String.valueOf(i);
+			levelRepository.save(level);
+		}
 	}
 }
--- src/main/java/edu/kidswhocode/studentmgmt/repository/StudentRepository.java
+++ src/main/java/edu/kidswhocode/studentmgmt/repository/StudentRepository.java
@@ -3,5 +3,5 @@ package edu.kidswhocode.studentmgmt.repository;
 import edu.kidswhocode.studentmgmt.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Long, Student> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
